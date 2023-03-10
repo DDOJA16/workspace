0a7d467fb67739eda3def9a2848ecd8d19acfb4c
Bolla <skqsk3@naver.com>
2023-03-10 16:08:20 +0900

    ☁ json-server-auth 회원가입 기능 추가

--- 0306_JWT/client/src/pages/signUpForm.js
+++ 0306_JWT/client/src/pages/signUpForm.js
@@ -1,12 +1,16 @@
 import React, { useState } from "react";
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import { useCookies } from "react-cookie";
 
 function Register() {
+  const [cookies, setCookies, removeCookies] = useCookies();
   const [inputValue, setInputValue] = useState({
     email: "",
     password: "",
   });
 
+  const navigate = useNavigate();
   const inputChangeHandler = (e) => {
     const { name, value } = e.target;
     setInputValue({
@@ -19,6 +23,10 @@ function Register() {
     try {
       const { data } = await axios.post("http://localhost:4000/register", inputValue);
       console.log(data);
+      // 쿠키추가 (path:"/" -> 전체 웹사이트에서 사용가능)
+      setCookies("accessToken", data["accessToken"], { path: "/" });
+      // 회원가입 성공시 메인페이지 이동
+      navigate("/");
     } catch (error) {
       console.log(error);
     }
